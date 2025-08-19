26032ceb881cce89fe226c21ac539f115513c345
Matthew Noto <notomatthew31@gmail.com>
2025-08-19 14:17:48 -0700

    test

--- fastvideo/layers/linear.py
+++ fastvideo/layers/linear.py
@@ -233,9 +233,8 @@ class ReplicatedLinear(LinearBase):
     def forward(self, x: torch.Tensor) -> tuple[torch.Tensor, Parameter | None]:
         bias = self.bias if not self.skip_bias_add else None
         assert self.quant_method is not None
-        print(f"input shape: {x.shape}")
         output = self.quant_method.apply(self, x, bias)
-        print(f"output shape: {output.shape}")
+        print(f"input shape: {x.shape} --> output shape: {output.shape}")
         output_bias = self.bias if self.skip_bias_add else None
         return output, output_bias
 
